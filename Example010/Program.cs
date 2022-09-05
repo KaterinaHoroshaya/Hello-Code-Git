be680544b6f4fd8fb39ac795e502220471552b0c
Katerina <m-a-c-u-k-a@mail.ru>
2022-09-05 21:56:43 +0700

    массив

---
+++ Example010/Program.cs
@@ -0,0 +1,15 @@
+﻿int [] array={1, 12, 31, 17, 24, 56, 36, 17, 80};
+
+int n = array.Length;
+int find = 17;
+int index = 0;
+while (index < n)
+{
+    if(array[index] == find)
+    {
+        Console.WriteLine(index);
+        break;
+    }
+    //index = index + 1;
+    index++;
+}
